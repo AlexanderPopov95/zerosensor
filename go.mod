b325e1d69c3b3dfa3d7d30b7065aea4ab544ae3e
Alexander Popov <popov.psk@gmail.com>
2021-04-01 11:30:35 +0300

    init

---
+++ go.mod
@@ -0,0 +1,9 @@
+module zerosensor
+
+go 1.16
+
+require (
+	github.com/MichaelS11/go-dht v0.1.0
+	github.com/prometheus/client_golang v1.10.0
+	periph.io/x/periph v3.6.7+incompatible // indirect
+)
---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/MichaelS11/go-dht"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+const (
+	addr = ":8081"
+	pin  = "GPIO17"
+)
+
+func main() {
+	mainRoomTemp := prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "main_room_temp",
+	})
+	mainRoomHumidity := prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "main_room_humidity",
+	})
+
+	prometheus.MustRegister(mainRoomTemp)
+	prometheus.MustRegister(mainRoomHumidity)
+
+	err := dht.HostInit()
+	if err != nil {
+		panic(err)
+	}
+
+	dht22, err := dht.NewDHT(pin, dht.Celsius, "")
+	if err != nil {
+		fmt.Println("NewDHT error:", err)
+		return
+	}
+
+	stop := make(chan struct{})
+	stopped := make(chan struct{})
+	var humidity float64
+	var temperature float64
+
+	go dht22.ReadBackground(&humidity, &temperature, time.Second*5, stop, stopped)
+	go func() {
+		for {
+			<-time.After(time.Second * 5)
+			mainRoomTemp.Set(temperature)
+			mainRoomHumidity.Set(humidity)
+		}
+	}()
+
+	http.Handle("/metrics", promhttp.Handler())
+	err = http.ListenAndServe(addr, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("server started on %s\n", addr)
+
+	term := make(chan os.Signal)
+	signal.Notify(term, syscall.SIGINT, syscall.SIGTERM)
+	<-term
+	fmt.Println("stopping...")
+	close(stop)
+	<-stopped
+	fmt.Println("successfully stopped =)")
+}
