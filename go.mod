f52958c8f0ced9e8f92e281623bea795f14765c5
Alexander Popov <popovaleksa@ozon.ru>
2021-05-07 11:45:04 +0300

    added mhz19b sensor

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.16
 require (
 	github.com/MichaelS11/go-dht v0.1.0
 	github.com/prometheus/client_golang v1.10.0
+	github.com/tarm/serial v0.0.0-20180830185346-98f6abe2eb07
 	periph.io/x/periph v3.6.7+incompatible // indirect
 )
--- main.go
+++ main.go
@@ -12,11 +12,13 @@ import (
 	"github.com/MichaelS11/go-dht"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/tarm/serial"
 )
 
 const (
-	addr = ":8081"
-	pin  = "GPIO17"
+	addr    = ":8081"
+	pin     = "GPIO17"
+	serial0 = "/dev/serial0"
 )
 
 type Temp struct {
@@ -25,31 +27,27 @@ type Temp struct {
 }
 
 func main() {
-	mainRoomTemp := prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "main_room_temp",
-	})
-	mainRoomHumidity := prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "main_room_humidity",
-	})
-
+	mainRoomTemp := prometheus.NewGauge(prometheus.GaugeOpts{Name: "main_room_temp"})
+	mainRoomHumidity := prometheus.NewGauge(prometheus.GaugeOpts{Name: "main_room_humidity"})
+	mainRoomCO2 := prometheus.NewGauge(prometheus.GaugeOpts{Name: "main_room_co2"})
 	prometheus.MustRegister(mainRoomTemp)
 	prometheus.MustRegister(mainRoomHumidity)
-
-	err := dht.HostInit()
-	if err != nil {
-		panic(err)
-	}
-
-	dht22, err := dht.NewDHT(pin, dht.Celsius, "")
-	if err != nil {
-		fmt.Println("NewDHT error:", err)
-		return
-	}
+	prometheus.MustRegister(mainRoomCO2)
 
 	stop := make(chan struct{})
 	stopped := make(chan struct{})
 	var humidity float64
 	var temperature float64
+
+	err := dht.HostInit()
+	if err != nil {
+		panic(err)
+	}
+	dht22, err := dht.NewDHT(pin, dht.Celsius, "")
+	if err != nil {
+		fmt.Println("NewDHT error:", err)
+		return
+	}
 
 	go dht22.ReadBackground(&humidity, &temperature, time.Second*5, stop, stopped)
 	go func() {
@@ -69,6 +67,7 @@ func main() {
 		writer.Write(js)
 		writer.WriteHeader(200)
 	})
+	go startMhz19b(mainRoomCO2)
 	go func() {
 		err = http.ListenAndServe(addr, nil)
 		if err != nil {
@@ -85,4 +84,29 @@ func main() {
 	close(stop)
 	<-stopped
 	fmt.Println("successfully stopped =)")
+}
+
+func startMhz19b(metric prometheus.Gauge) {
+	c := &serial.Config{Name: serial0, Baud: 9600, Size: 8, Parity: 0, StopBits: 1}
+	port, err := serial.OpenPort(c)
+	if err != nil {
+		panic(err)
+	}
+	time.Sleep(time.Second)
+	defer port.Close()
+	_, err = port.Write([]byte{0xFF, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79})
+	if err != nil {
+		panic(err)
+	}
+	for {
+		buf := make([]byte, 9)
+		_, err = port.Read(buf)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		result := int(buf[2])*256 + int(buf[3])
+		metric.Set(float64(result))
+		fmt.Println(result)
+	}
 }
